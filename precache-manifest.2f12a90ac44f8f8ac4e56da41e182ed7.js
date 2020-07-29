c9f2ed18ec1d97ee0a9d3ca7d4dbf6716223f92d
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-07-29 18:57:45 -0500

    Updates

---
+++ precache-manifest.2f12a90ac44f8f8ac4e56da41e182ed7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcaed0a30aed7aa0c9c0b7db1d717d7a",
+    "url": "/Ely_Flores/index.html"
+  },
+  {
+    "revision": "275ba505e58920655a0c",
+    "url": "/Ely_Flores/static/css/main.a9e62db5.chunk.css"
+  },
+  {
+    "revision": "fe2ce23bb55b3dec1559",
+    "url": "/Ely_Flores/static/js/2.e3263c36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Ely_Flores/static/js/2.e3263c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275ba505e58920655a0c",
+    "url": "/Ely_Flores/static/js/main.5189afe6.chunk.js"
+  },
+  {
+    "revision": "b86f15cc9c3dbd92a56d",
+    "url": "/Ely_Flores/static/js/runtime-main.4ab653be.js"
+  },
+  {
+    "revision": "303cd5c4e2169130a9ddd90bfab76982",
+    "url": "/Ely_Flores/static/media/Burguer.303cd5c4.png"
+  },
+  {
+    "revision": "04155e6bf44217c92249711d1c1567f3",
+    "url": "/Ely_Flores/static/media/Rick.04155e6b.png"
+  },
+  {
+    "revision": "a67b6412381931eadad3cca98b1b8050",
+    "url": "/Ely_Flores/static/media/Sanford-0103_demo.a67b6412.ttf"
+  },
+  {
+    "revision": "6b0e57d49b0161333750943633a34eeb",
+    "url": "/Ely_Flores/static/media/TripLife.6b0e57d4.png"
+  },
+  {
+    "revision": "76af6f346e8c2cb1b1415ecc813c9be8",
+    "url": "/Ely_Flores/static/media/bicicleta.76af6f34.png"
+  },
+  {
+    "revision": "43b2bd8f21abdd5dda6a0ffca73129ee",
+    "url": "/Ely_Flores/static/media/violence.43b2bd8f.png"
+  }
+]);
